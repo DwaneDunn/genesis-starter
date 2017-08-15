54db80d294481684efce8edec9bf0f0a64c304b0
Dwane Dunn <Dwane.Dunn@gmail.com>
2017-08-15 13:53:53 -0400

    Added unregister function to sidebar.php

--- lib/structure/sidebar.php
+++ lib/structure/sidebar.php
@@ -3,10 +3,21 @@
  * Sidebar (Widgetized areas) HTML markup structure
  *
  * @package     KnowITMedia\GenesisStarter
- * @since       1.0.0
+ * @since       1.0.1
  * @author      Dwane Dunn
  * @link        https://dwanedunn.com
  * @license     GNU General Public License 2.0+
  */
 
 namespace KnowITMedia\GenesisStarter;
+
+/**
+ * Unregister sidebar callbacks.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function unregister_sidebar_callbacks() {
+
+}
