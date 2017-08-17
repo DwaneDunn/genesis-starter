8b33c0cd6e1f30d6abbe41380364bd1b2853c2a3
Dwane Dunn <Dwane.Dunn@gmail.com>
2017-08-17 19:56:58 -0400

    Setup initial gulp config, package.json and gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,31 @@
 var gulp = require('gulp'),
     bourbon = require('bourbon').includePaths,
     neat = require('bourbon-neat').includePaths,
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', function(){
 
    return gulp.src('assets/sass/style.scss')
+
+       .pipe( sourcemaps.init() )
+
        .pipe( sass({
            includePaths: [].concat( bourbon, neat ),
            errorLogToConsole: true,
            outputStyle: 'expanded' // Options: nested, expanded, compact, compressed
        }) )
+
+       .pipe( postcss([
+           autoprefixer({
+               browsers: ['last 2 versions']
+           })
+       ]) )
+
+       .pipe( sourcemaps.write() )
+
        .pipe(gulp.dest('./'));
 });
 
