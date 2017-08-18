637737965da29e3577d3a39ef78f55dc32b1a9a4
Dwane Dunn <Dwane.Dunn@gmail.com>
2017-08-18 13:28:48 -0400

    Completed gulpfile.js and setup tasks

--- gulpfile.js
+++ gulpfile.js
@@ -11,14 +11,57 @@ var gulp = require('gulp'),
     //mqpacker = require('css-mqpacker'),
     sourcemaps = require('gulp-sourcemaps'),
     cssnano = require('gulp-cssnano'),
+    sassLint = require('gulp-sass-lint'),
 
     // utilities
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber');
+
+
+/*****************************************************
+ * Utilities
+ ******************/
+
+/**
+ * Error handling
+ *
+ * @function
+ */
+function handleErrors() {
+    var args = Array.prototype.slice.call(arguments);
+
+    notify.onError({
+        title: 'Task Failed [<%= error.message %>]',
+        message: 'See console.',
+        sound: 'Sosumi' // See https://github.com/mikaelbr/node-notifier
+
+    }).apply(this, args);
+
+    gutil.beep(); // Beep 'sosumi' again
+
+    // Prevent the 'watch' task from stopping
+    this.emit('end');
+}
+
+/**********************************************************
+ * CSS Tasks
+ **********************************************************/
+
+/**
+ * PostCSS Task Handler
+ */
 
 gulp.task('postcss', function(){
 
    return gulp.src('assets/sass/style.scss')
 
+       // Error handling
+       .pipe( plumber({
+           errorHandler: handleErrors
+       }) )
+
+       // Sourcemap wrapping
        .pipe( sourcemaps.init() )
 
        .pipe( sass({
@@ -38,22 +81,50 @@ gulp.task('postcss', function(){
 
        .pipe( sourcemaps.write() )
 
-       .pipe(gulp.dest('./'));
+       .pipe( gulp.dest('./') );
 
 }); // End of styles task
 
-gulp.task('cssMinify', ['postcss'], function () {
+gulp.task('css:minify', ['postcss'], function () {
 
     return gulp.src('style.css')
+
+        // Error handling
+        .pipe( plumber({
+            errorHandler: handleErrors
+        }) )
+
         .pipe( cssnano({
             safe: true
         }) )
 
         .pipe(rename('style.min.css'))
 
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./'))
+
+        .pipe( notify({
+            message: 'Styles are built.'
+        }) );
 
 });
+
+gulp.task('sass:lint', ['css:minify'], function () {
+    gulp.src([
+        'assets/sass/style.scss',
+        // Exclude these assets
+        '!assets/sass/base/html5-reset/_normalize.scss',
+        '!assets/sass/utilities/animate/**/*.*'
+    ])
+        .pipe( sassLint() )
+        .pipe( sassLint.format() )
+        .pipe( sassLint.failOnError() );
+
+});
+
+/**
+ * All Task Listeners
+ */
+
 
 gulp.task('watch', function() {
 
@@ -67,4 +138,4 @@ gulp.task('watch', function() {
 
 // gulp.task( 'scripts', [''] )
 
-gulp.task( 'styles', ['cssMinify'] );
+gulp.task( 'styles', ['sass:lint'] );
