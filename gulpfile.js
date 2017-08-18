7737edd519de0e4ebb862a9b94603d6886e6a77d
Dwane Dunn <Dwane.Dunn@gmail.com>
2017-08-17 20:19:46 -0400

    Added gulp-cssnano and gulp-renane tasks to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 'use strict';
 
 var gulp = require('gulp'),
+
+    // Sass / CSS Processes
     bourbon = require('bourbon').includePaths,
     neat = require('bourbon-neat').includePaths,
     sass = require('gulp-sass'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
-    sourcemaps = require('gulp-sourcemaps');
+    //mqpacker = require('css-mqpacker'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cssnano = require('gulp-cssnano'),
+
+    // utilities
+    rename = require('gulp-rename');
 
 gulp.task('styles', function(){
 
@@ -24,13 +31,32 @@ gulp.task('styles', function(){
            autoprefixer({
                browsers: ['last 2 versions']
            })
+           // mqpacker({
+           //     sort: true
+           // })
        ]) )
 
        .pipe( sourcemaps.write() )
 
        .pipe(gulp.dest('./'));
+
+}); // End of styles task
+
+gulp.task('cssMinify', ['styles'], function () {
+
+    return gulp.src('style.css')
+        .pipe( cssnano({
+            safe: true
+        }) )
+
+        .pipe(rename('style.min.css'))
+
+        .pipe(gulp.dest('./'));
+
 });
 
 gulp.task('watch', function() {
+
    gulp.watch('assets/sass/**/*.scss', ['styles']);
+
 });
